00659781fe95e00c136fd82d6db88559865d4b6e
Sam2959 <l.shyamsukumar@gmail.com>
2024-04-28 16:52:33 +0530

    add bcrypt auth function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: "postgres",
@@ -39,11 +41,18 @@ app.post("/register", async (req, res) => {
     if (checkEmail.rows.length > 0) {
       res.send("Email. already exists. Try loggin in...");
     } else {
-      await db.query("insert into users(email,password)values($1,$2)", [
-        email,
-        password,
-      ]);
-      res.render("secrets.ejs");
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [email, hash]
+          );
+          console.log(result);
+          res.render("secrets.ejs");
+        }
+      });
     }
   } catch (err) {
     console.log(err);
@@ -52,19 +61,28 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const loginpassword = req.body.password;
   try {
     const checkEmail = await db.query("select * from users where email=$1", [
       email,
     ]);
     if (checkEmail.rows.length > 0) {
-      const result = await db.query("select email,password from users");
-      if (
-        email === result.rows[0].email &&
-        password === result.rows[0].password
-      ) {
-        res.render("secrets.ejs");
-      }
+      const result = await db.query(
+        "select password from users where email=$1",
+        [email]
+      );
+      const storedHashedPassword = result.rows[0].password;
+      console.log(result.rows);
+      bcrypt.compare(loginpassword, storedHashedPassword, (err, result) => {
+        console.log(result);
+        if (err) {
+          console.log(err);
+        } else if (result) {
+          res.render("secrets.ejs");
+        } else {
+          res.send("Incorrect Password");
+        }
+      });
     } else {
       res.send("User not found, Please register..");
     }

3d30cdd76583665a71b37de2f0b43b3262163c93
Sam2959 <l.shyamsukumar@gmail.com>
2024-05-01 22:23:38 +0530

    final commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import pg from "pg";
 import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
-import GoogleStrategy from "passport-google-oauth2";
 
 const app = express();
 const port = 3000;
@@ -20,7 +20,6 @@ app.use(
     saveUninitialized: true,
   })
 );
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -57,9 +56,37 @@ app.get("/logout", (req, res) => {
   });
 });
 
-app.get("/secrets", (req, res) => {
+app.get("/secrets", async (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("secrets.ejs");
+    try {
+      const result = await db.query("select secret from users where email=$1", [
+        req.user.email,
+      ]);
+      console.log(result.rows[0].secret);
+      if (result.rows[0].secret) {
+        res.render("secrets.ejs", {
+          secret: result.rows[0].secret,
+        });
+      } else {
+        res.render("secrets.ejs", {
+          secret: "Jack Bauer is my hero.",
+        });
+      }
+    } catch (err) {
+      console.log(error);
+    }
+
+    //TODO: Update this to pull in the user secret to render in secrets.ejs
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//TODO: Add a get route for the submit button
+//Think about how the logic should work with authentication.
+app.get("/submit", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("submit.ejs");
   } else {
     res.redirect("/login");
   }
@@ -71,6 +98,7 @@ app.get(
     scope: ["profile", "email"],
   })
 );
+
 app.get(
   "/auth/google/secrets",
   passport.authenticate("google", {
@@ -82,7 +110,7 @@ app.get(
 app.post(
   "/login",
   passport.authenticate("local", {
-    successRedirect: "/secrets",
+    successRedirect: ["/secrets"],
     failureRedirect: "/login",
   })
 );
@@ -120,6 +148,24 @@ app.post("/register", async (req, res) => {
   }
 });
 
+//TODO: Create the post route for submit.
+//Handle the submitted data and add it to the database
+app.post("/submit", async function (req, res) {
+  console.log(req.user);
+  const secret = req.body.secret;
+  if (req.isAuthenticated()) {
+    try {
+      await db.query(`UPDATE users SET secret = $1 WHERE email = $2`, [
+        secret,
+        req.user.email,
+      ]);
+      res.redirect("/secrets");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
 passport.use(
   "local",
   new Strategy(async function verify(username, password, cb) {
@@ -132,15 +178,12 @@ passport.use(
         const storedHashedPassword = user.password;
         bcrypt.compare(password, storedHashedPassword, (err, valid) => {
           if (err) {
-            //Error with password check
             console.error("Error comparing passwords:", err);
             return cb(err);
           } else {
             if (valid) {
-              //Passed password check
               return cb(null, user);
             } else {
-              //Did not pass password check
               return cb(null, false);
             }
           }
@@ -165,12 +208,13 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
-        const result = await db.query("select * from users where email=$1", [
+        // console.log(profile);
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [
           profile.email,
         ]);
         if (result.rows.length === 0) {
           const newUser = await db.query(
-            "insert into users(email,password) values($1,$2)",
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
             [profile.email, "google"]
           );
           return cb(null, newUser.rows[0]);
@@ -183,10 +227,10 @@ passport.use(
     }
   )
 );
-
 passport.serializeUser((user, cb) => {
   cb(null, user);
 });
+
 passport.deserializeUser((user, cb) => {
   cb(null, user);
 });
